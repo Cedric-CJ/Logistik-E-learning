7c66f251a4a7b21954b9a270cd141ebcf7231220
Cedric Arnhold <Cedric.Arnhold@Student.HTW-Berlin.de>
2025-07-21 15:32:45 +0200

    3.ter versuch

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
+  const isProduction = mode === 'production'
   
   return {
     plugins: [
@@ -17,7 +15,7 @@ export default defineConfig(({ command, mode }) => {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       },
     },
-    base: process.env.NODE_ENV === 'production' ? '/Logistik-E-learning/' : '/',
+    base: isProduction ? '/Logistik-E-learning/' : '/',
     build: {
       outDir: 'dist',
       assetsDir: 'assets',
@@ -25,11 +23,14 @@ export default defineConfig(({ command, mode }) => {
       emptyOutDir: true,
       rollupOptions: {
         output: {
-          assetFileNames: 'assets/[name]-[hash][extname]',
           entryFileNames: 'assets/[name]-[hash].js',
-          chunkFileNames: 'assets/[name]-[hash].js'
+          chunkFileNames: 'assets/[name]-[hash].js',
+          assetFileNames: 'assets/[name]-[hash][extname]',
         }
       }
+    },
+    server: {
+      port: 3000
     }
   }
 })
